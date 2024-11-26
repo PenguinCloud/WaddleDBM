ed6f6ab9b790b01fe0e7521c97ca0e63e67cbb5b
thysw95 <thys103@gmail.com>
2024-11-26 12:06:09 +0200

    Fixed alot of import issues after moving waddlelibs to the modules directory

--- models/db.py
+++ models/db.py
@@ -12,11 +12,11 @@ import sys
 from applications.WaddleDBM.scripts.init_db import db_initializer
 
 # Import the the waddledbm_helpers class from the WaddleDBM botDBMHelpers scripts module
-from applications.WaddleDBM.WaddlebotLibs.botDBMHelpers import dbm_helpers
+from WaddlebotLibs.botDBMHelpers import dbm_helpers
 # from ..WaddlebotLibs.botDBMHelpers import dbm_helpers
 
 # Import the matterbridge_helpers class from the WaddleDBM botMatterbridgeHelpers scripts module
-from applications.WaddleDBM.WaddlebotLibs.botMatterbridgeHelpers import matterbridge_helpers
+from WaddlebotLibs.botMatterbridgeHelpers import matterbridge_helpers
 # from ..WaddlebotLibs.botMatterbridgeHelpers import matterbridge_helpers
 
 # -------------------------------------------------------------------------
--- scripts/init_db.py
+++ scripts/init_db.py
@@ -7,8 +7,8 @@ import logging
 
 # Import the necessary dataclasses from the Waddlebot-libs module
 # Might need to change the name of the module to Waddlebot_libs, because python is case sensitive
-from applications.WaddleDBM.WaddlebotLibs.botClasses import module
-from applications.WaddleDBM.WaddlebotLibs.botClasses import module_command_metadata
+from WaddlebotLibs.botClasses import module
+from WaddlebotLibs.botClasses import module_command_metadata
 
 # Set the logging level to INFO
 logging.basicConfig(level=logging.INFO)
